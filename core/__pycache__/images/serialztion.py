4c8761ba6756cebd8777fdc23c37851c336185d2
Ahmed Khaled Askar <102247707+Askarjr@users.noreply.github.com>
2022-08-03 21:32:02 +0200

    Delete core/__pycache__ directory

--- core/__pycache__/images/serialztion.py
+++
@@ -1,9 +0,0 @@
-from unicodedata import category
-from rest_framework import serializers
-from .models import Item
-
-class ImagesofItem(serializers.ModelSerializer):
-    class Meta:
-        model=Item
-        fields='__all__'
-
